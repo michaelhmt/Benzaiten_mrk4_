9f57e6ae8a469f792922c70819e9ca7d23c8292a
michaelhmt <hindleymht@gmail.com>
2021-11-24 07:51:07 +0000

    updated constants

--- venv/Benzaiten_Common/Benzaiten_constants.py
+++ venv/Benzaiten_Common/Benzaiten_constants.py
@@ -5,5 +5,9 @@ STORY_PAGE_CONSTANT = 'https://archiveofourown.org{url}?view_adult=true">Proceed
 STORY_INDEX_CONSTANT = "https://archiveofourown.org{url}/navigate"
 SEARCHPAGE_CONSTANT = 'https://archiveofourown.org/tags/{Fandom_Source}/works?page={}'
 
+VIEW_ALL_CONSTANT ="https://archiveofourown.org{url}?view_adult=true&view_full_work=true" #MIGHT THROW AN ERROR WITH MATURE CONTENT
+
+COOKIES_CONSTANT = {'domain': 'archiveofourown.org', 'httpOnly': False, 'name': 'view_adult', 'path': '/', 'secure': False, 'value': 'true'}
+
 DRIVER_PATH = 'E:\\Python\\Benzaiten_mrk4\\chromedriver.exe' #the path where you have "chromedriver" file.
 INGESTED_LOG = 'E:\\Python\\Benzaiten_mrk4\\venv\\Benzaiten_Common\\Ingested_Log_{Fandom_Name}.json'
